7845e0d695275c0fdb83cc70b3dbe4ac31126ec4
Nicolas Bigaouette <nbigaouette@gmail.com>
2020-05-20 17:40:49 -0400

    Add main file exposing the problem

---
+++ src/main.rs
@@ -0,0 +1,29 @@
+use std::{env, thread, time::Duration};
+
+use ldap3::{LdapConn, LdapConnSettings};
+
+fn main() {
+    let url = env::var("LDAP3_ISSUE47_URL").unwrap();
+    let bind_dn = env::var("LDAP3_ISSUE47_BIND_DN").unwrap();
+    let bind_pw = env::var("LDAP3_ISSUE47_BIND_PW").unwrap();
+
+    loop {
+        println!("Establishing connection to {:?}...", url);
+
+        let timeout = Duration::from_secs(5);
+        let settings = LdapConnSettings::new()
+            .set_conn_timeout(timeout)
+            .set_no_tls_verify(true);
+
+        let conn = LdapConn::with_settings(settings, url.as_str()).unwrap();
+        println!("    Binding using {:?}...", bind_dn);
+        let bind = conn.simple_bind(&bind_dn, &bind_pw).unwrap();
+
+        println!("    Connection established. Dropping.");
+
+        std::mem::drop(bind);
+        std::mem::drop(conn);
+
+        thread::sleep(Duration::from_millis(500));
+    }
+}
