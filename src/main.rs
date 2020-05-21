598b769e7cbf2c728ed9a025b9114f021d31e13e
Nicolas Bigaouette <nbigaouette@gmail.com>
2020-05-21 11:59:52 -0400

    Decrease loop period for more connections, no need to bind to expose problem

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,6 @@ use ldap3::{LdapConn, LdapConnSettings};
 
 fn main() {
     let url = env::var("LDAP3_ISSUE47_URL").unwrap();
-    let bind_dn = env::var("LDAP3_ISSUE47_BIND_DN").unwrap();
-    let bind_pw = env::var("LDAP3_ISSUE47_BIND_PW").unwrap();
 
     loop {
         println!("Establishing connection to {:?}...", url);
@@ -16,14 +14,11 @@ fn main() {
             .set_no_tls_verify(true);
 
         let conn = LdapConn::with_settings(settings, url.as_str()).unwrap();
-        println!("    Binding using {:?}...", bind_dn);
-        let bind = conn.simple_bind(&bind_dn, &bind_pw).unwrap();
 
         println!("    Connection established. Dropping.");
 
-        std::mem::drop(bind);
         std::mem::drop(conn);
 
-        thread::sleep(Duration::from_millis(500));
+        thread::sleep(Duration::from_millis(100));
     }
 }
